1cdd34c72af4ac82a664a4a05b4d3109279afa43
thu-vu-33 <80305944+thu-vu-33@users.noreply.github.com>
2025-05-24 21:13:45 +0700

    Update date.go

--- api/date.go
+++ api/date.go
@@ -1,12 +1,48 @@
+// package handler
+
+// import (
+// 	"fmt"
+// 	"net/http"
+// 	"time"
+// )
+
+// func Date(w http.ResponseWriter, r *http.Request) {
+// 	currentTime := time.Now().Format(time.RFC850)
+// 	fmt.Fprint(w, currentTime)
+// }
+
+
 package handler
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
-	"time"
 )
 
-func Date(w http.ResponseWriter, r *http.Request) {
-	currentTime := time.Now().Format(time.RFC850)
-	fmt.Fprint(w, currentTime)
+func GetFileContent(w http.ResponseWriter, r *http.Request) {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "https://1fichier.com/?bopqkt2nz13mv3fvk3n0", nil)
+	if err != nil {
+		fmt.Fprint(w, "Error creating request: ", err)
+		return
+	}
+
+	req.Header.Set("Cookie", "show_cm=no; SID=EesWwrKekPA0Koh14igcheGxfEqNSZQwrrcwVxMpotRpQt9=uWK9eX3l4o2KIaym")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Fprint(w, "Error sending request: ", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Fprint(w, "Error reading response: ", err)
+		return
+	}
+
+	fmt.Fprint(w, string(body))
 }
+
